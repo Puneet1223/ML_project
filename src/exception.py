446f70c97bc76da7e9f33d40b2bb141ac93a1460
Puneet1223 <puneetrajpoot513@gmail.com>
2026-03-02 13:40:00 +0530

    Model Trainer

--- src/exception.py
+++ src/exception.py
@@ -2,7 +2,7 @@ import sys
 from src.logger import logging
 
 def error_message_detail(error, error_detail:sys): 
-    _,_,exe_tb=error_detail.exe_info()
+    _,_,exe_tb=error_detail.exc_info()
     file_name = exe_tb.tb_frame.f_code.co_filename
     error_message = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(exe_tb.tb_frame.f_code.co_filename,exe_tb.tb_lineno,str(error))
     return error_message
@@ -11,7 +11,7 @@ def error_message_detail(error, error_detail:sys):
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):   
         super().__init__(error_message)
-        self.error_message = error_message_detail(error_message,error_details=error_detail)
+        self.error_message = error_message_detail(error_message,error_detail)
 
     def __str__(self):
         return self.error_message    
